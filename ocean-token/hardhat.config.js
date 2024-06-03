b5b1d20805486a5267603d6b2e2fd8fbd20fb46d
TanmayMachkar <tanmaymacho@gmail.com>
2024-06-04 00:54:46 +0530

    deploy script half done and updated package.json

--- ocean-token/hardhat.config.js
+++ ocean-token/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
+  network: {
+    rinkeby: {
+      url: process.env.INFURA_RINKEBY_ENDPOINT,
+      account: [process.env.PRIVATE_KEY]
+    }
+  }
 };
---
+++ ocean-token/scripts/deploy.js
@@ -0,0 +1,10 @@
+const hre = require("hardhat");
+
+async function main(){
+	const OceanToken = await hre.ethers.getContractFactory
+}
+
+main().catch((error) => {
+	console.error(error);
+	process.exitCode = 1;
+});
