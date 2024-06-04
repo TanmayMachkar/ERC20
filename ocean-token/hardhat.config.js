0fd12aad8203ea7e25222165d32fbec3e8ca7380
TanmayMachkar <tanmaymacho@gmail.com>
2024-06-04 11:14:25 +0530

    oct token created prolly will be the final commit

--- ocean-token/hardhat.config.js
+++ ocean-token/hardhat.config.js
@@ -4,10 +4,12 @@ require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
-  network: {
-    rinkeby: {
-      url: process.env.INFURA_RINKEBY_ENDPOINT,
-      account: [process.env.PRIVATE_KEY]
+  networks: {
+    sepolia: {
+      url: process.env.INFURA_SEPOLIA_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };
+
+//0xEa6d30D0783971baEd1EBFF1f88abd7EDe30cea5
--- ocean-token/scripts/deploy.js
+++ ocean-token/scripts/deploy.js
@@ -1,7 +1,12 @@
 const hre = require("hardhat");
 
 async function main(){
-	const OceanToken = await hre.ethers.getContractFactory
+	const OceanToken = await hre.ethers.getContractFactory("OceanToken");
+	const oceanToken = await OceanToken.deploy(100000000, 50);
+
+	await oceanToken.deployed();
+
+	console.log("Ocean Token deployed: ", oceanToken.address);
 }
 
 main().catch((error) => {
